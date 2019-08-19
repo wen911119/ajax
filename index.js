b435d4a7a922b3c1a7fee83b724e20a41576d355
wen911119 <wen911119@gmail.com>
2019-08-19 14:40:44 +0800

    fix#兼容性

--- index.js
+++ index.js
@@ -1,74 +1,82 @@
-import axios from 'axios'
+import axios from "axios";
 
-const isCustomCatch = error => {
-  const catchConfig = error.config.headers.catch
+const isCustomCatch = function(error) {
+  const catchConfig = error.config.headers.catch;
   if (catchConfig) {
-    if (catchConfig === 'all') {
-      return true
+    if (catchConfig === "all") {
+      return true;
     } else {
-      const status = error.response && error.response.status
+      const status = error.response && error.response.status;
       if (status) {
         const needCatchedStatusCodes = catchConfig
-          .split(',')
-          .map(statusCodeStr => parseInt(statusCodeStr))
+          .split(",")
+          .map(function(statusCodeStr) {
+            return parseInt(statusCodeStr);
+          });
         if (needCatchedStatusCodes.includes(status)) {
-          return true
+          return true;
         }
       }
     }
   }
-  return false
-}
+  return false;
+};
 
-let defaultErrorHandler = error => {
-  console.warn('需要自定义错误处理函数')
-  console.log(error)
-}
+let defaultErrorHandler = function(error) {
+  console.warn("需要自定义错误处理函数");
+  console.log(error);
+};
 
 let Loading = {
-  show: () => {
-    console.warn('需要自定义loading.show方法')
+  show: function() {
+    console.warn("需要自定义loading.show方法");
   },
-  hide: () => {
-    console.warn('需要自定义loading.hide方法')
+  hide: function() {
+    console.warn("需要自定义loading.hide方法");
   }
-}
+};
 
-let onRequestHandler = config => {}
+let onRequestHandler = function(config) {};
 
-axios.interceptors.request.use(config => {
-  if (config.headers.loading !== 'false') {
-    config.headers.loadingId = Loading.show()
+axios.interceptors.request.use(function(config) {
+  if (config.headers.loading !== "false") {
+    config.headers.loadingId = Loading.show();
   }
-  onRequestHandler && onRequestHandler(config)
-  return config
-})
+  onRequestHandler && onRequestHandler(config);
+  return config;
+});
 
 axios.interceptors.response.use(
-  response => {
-    if (response.config.headers.loading !== 'false') {
-      Loading.hide(response.config.headers.loadingId)
+  function(response) {
+    if (response.config.headers.loading !== "false") {
+      Loading.hide(response.config.headers.loadingId);
     }
-    return response.data
+    return response.data;
   },
-  error => {
-    if (error.config.headers.loading !== 'false') {
-      Loading.hide(error.config.headers.loadingId)
+  function(error) {
+    if (error.config.headers.loading !== "false") {
+      Loading.hide(error.config.headers.loadingId);
     }
     if (isCustomCatch(error)) {
       // 用户希望自己处理异常
-      return Promise.reject(error)
+      return Promise.reject(error);
     } else {
       // 默认错误处理
-      defaultErrorHandler(error)
+      defaultErrorHandler(error);
       // 终止promise链
-      return new Promise(() => {})
+      return new Promise(function() {});
     }
   }
-)
+);
 
-export default axios
+export default axios;
 
-export const setDefaultErrorHander = handler => (defaultErrorHandler = handler)
-export const setLoadingMethod = loadingMethod => (Loading = loadingMethod)
-export const setOnRequestHandler = handler => (onRequestHandler = handler)
+export const setDefaultErrorHander = function(handler) {
+  defaultErrorHandler = handler;
+};
+export const setLoadingMethod = function(loadingMethod) {
+  Loading = loadingMethod;
+};
+export const setOnRequestHandler = function() {
+  onRequestHandler = handler;
+};
